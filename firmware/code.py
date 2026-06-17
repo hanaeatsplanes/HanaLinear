9b6529409b1cf0f2542b3b3ae2f089835f51928d
sahana <github@sahana.dev>
2026-06-17 19:44:36 -0400

    oops

--- firmware/code.py
+++ firmware/code.py
@@ -1,17 +1,28 @@
 print("starting!!!")
 import board
 
-from kmk.scanners import DiodeOrientation
 
-row_pins = (board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6)
-col_pins = (board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18, board.GP19, board.GP20, board.GP21) 
-diode_orientation = DiodeOrientation.ROW2COL
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+from kmk.modules.encoder import EncoderHandler
+from kmk.modules.macros import Macros
+from kmk.extensions.media_keys import MediaKeys
+
+
+keyboard = KMKKeyboard()
+encoder_handler = EncoderHandler()
+macros = Macros()
+keyboard.modules = [macros, encoder_handler]
+keyboard.extensions.append(MediaKeys())
 
 # because i cant like. actually send fn. we're gonna replace fn with an emoji key. 
 EMOJI = KC.MACRO(KC.LGUI(KC.LCTL(KC.SPC)))
 
-
-keymap = [
+keyboard.row_pins = (board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6)
+keyboard.col_pins = (board.GP7, board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17, board.GP18, board.GP19, board.GP20, board.GP21)
+keyboard.diode_orientation = DiodeOrientation.ROW2COL
+keyboard.keymap = [
 [KC.ESC, KC.NO, KC.NO, KC.F3, KC.F4, KC.NO, KC.NO, KC.F7, KC.F8, KC.F9, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO],
 [KC.GRAVE, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.MINUS, KC.EQUAL, KC.NO, KC.BSPACE],
 [KC.TAB, KC.NO, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRACKET, KC.RBRACKET, KC.BSLASH],
@@ -19,3 +30,9 @@ keymap = [
 [KC.NO, KC.LSHIFT, KC.NO, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.N, KC.M, KC.COMMA, KC.DOT, KC.SLASH, KC.NO, KC.RSHIFT],
 [EMOJI, KC.LCTRL, KC.LALT, KC.LGUI, KC.NO, KC.NO, KC.NO, KC.SPACE, KC.NO, KC.NO, KC.NO, KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT]
 ]
+
+encoder_handler.pins = ((board.GP22, board.GP23, None), (board.GP24, board.GP25, board.GP26))
+encoder_handler.map = [((KC.BRIU, KC.BRID, KC.NO), (KC.VOLU, KC.VOLD, KC.MUTE))]
+
+if __name__ == "__main__":
+  keyboard.go()
